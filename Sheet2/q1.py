87ec86d31bb40b516cd83e025300464cad0eac81
Thomas Preston <thomas.diamond.dude@gmail.com>
2025-11-10 17:04:48 +0000

    q1 initial commit

---
+++ Sheet2/q1.py
@@ -0,0 +1,15 @@
+import numpy
+def primes(N):
+    prime = numpy.array([True for n in range(N+1)]) # This generates an array where prime[index] returns whether index is prime or not
+    prime[0]=prime[1]=False # Settting 0 and 1 to not prime
+    n=2
+    counter = 0
+    while n**2<=N:
+        possiblePrimes=numpy.where(prime == True)[0]
+        n=possiblePrimes[counter]
+        for i in range(2*n,N+1,n):
+            prime[i]=False
+        counter+=1
+    return numpy.where(prime == True)[0]
+N=1000000
+print(primes(N))
